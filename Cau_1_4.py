f6be2278e0d4a623bd859138bc312ca09e5c4146
hoanghoatham03 <153010793+hoanghoatham03@users.noreply.github.com>
2024-06-19 21:35:42 +0700

    Add files via upload

---
+++ Cau_1_4.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Playground')
+st.text_area('Input prompt', '')
+st.button('Submit')
+st.slider('Temperature', 0.00, 2.00, 1.00)
+st.slider('Top P', 0.00, 1.00, 1.00)
+st.slider('Maximum length', 1, 4000, 1)
+st.checkbox('Show probabilities')
+st.code("print('Hello world')", language='python', line_numbers=True)
+
+
+# Link web: https://21522120-cau1-4.streamlit.app/
